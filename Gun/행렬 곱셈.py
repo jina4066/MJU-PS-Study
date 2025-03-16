d8003051f2efefe994c477f90248cddd322ef9d1
Gopistol <104254012+Gopistol@users.noreply.github.com>
2025-03-16 13:36:21 +0000

    [FIX] 행렬 곱셈 Sol2 추가

--- Gun/행렬 곱셈.py
+++ Gun/행렬 곱셈.py
@@ -21,3 +21,7 @@ def solution(arr1, arr2):
             answer[col][row] = multiple(arr1[col], new[row])
 
     return answer
+
+# Sol2
+def productMatrix(A, B):
+    return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
